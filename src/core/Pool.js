b350c4c6b86a6f21037275e5cf9cc458f6b45c0a
羅子淇 Jacky Law, F.I.H.R.M.(HK), FHKIoD <289607996+jackylawck@users.noreply.github.com>
2026-08-26 17:52:47 +0800

    Update Pool.js

--- src/core/Pool.js
+++ src/core/Pool.js
@@ -1,9 +1,11 @@
+import * as THREE from 'three';
+
 export const POOL = {
     v1: new THREE.Vector3(),
     v2: new THREE.Vector3(),
+    v3: new THREE.Vector3(),
+    q1: new THREE.Quaternion(),
+    m1: new THREE.Matrix4(),
     forward: new THREE.Vector3(),
-    right: new THREE.Vector3(),
-    toEnd: new THREE.Vector3(),
-    toTarget: new THREE.Vector3(),
-    cross: new THREE.Vector3()
+    right: new THREE.Vector3()
 };
